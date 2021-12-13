c97dee27323d46abb7d9e9f4bdcb143fd20aa529
bryanthowell-ts <bryant.howell@thoughtspot.com>
2021-12-13 07:39:22 -0600

    Implemented batching for permissions (in 20s by default), which was breaking on a certain size of object list

--- examples/audit_object_access.py
+++ examples/audit_object_access.py
@@ -27,8 +27,35 @@ def create_id_name_dict(listobjectheaders_response):
 
 def get_permissions_for_all_objects(object_type, listobjectheaders_response, permission_type='DEFINED', dependent_share=True):
     id_map = create_id_name_dict(listobjectheaders_response)
-    perms = ts.tsrest.security_metadata_permissions(object_type=object_type, object_guids=list(id_map.keys()),
+    # Because the GUIDs pass in the URL, there is a limit to how many can be requested at a time
+    # Must batch through the full set, make whatever number of requests, then put the whole thing together
+    batch_size = 20
+    guids_list = list(id_map.keys())
+    # Reverse the list so that pop can run from the end of the list, more efficient
+    guids_list.reverse()
+    # Pop out the first elements in the batch size
+    guids_to_retrieve = []
+    for i in range(batch_size):
+        guids_to_retrieve.append(guids_list.pop())
+    # Get the first permissions object, we'll add the elements from subsequent calls to it
+    perms = ts.tsrest.security_metadata_permissions(object_type=object_type, object_guids=guids_to_retrieve,
                                                     dependent_share=dependent_share, permission_type=permission_type)
+    # The pop() action is reducing the list size so that eventually it will be at 0
+    while len(guids_list) > 0:
+        guids_to_retrieve = []
+        for i in range(batch_size):
+            # pop() throws IndexError once List is empty
+            try:
+                guids_to_retrieve.append(guids_list.pop())
+            # break the loop after the last item
+            except IndexError:
+                break
+        # Request the next batch of permissions by GUID
+        next_perms = ts.tsrest.security_metadata_permissions(object_type=object_type, object_guids=guids_to_retrieve,
+                                                    dependent_share=dependent_share, permission_type=permission_type)
+        for p in next_perms:
+            perms[p] = next_perms[p]
+
     return perms
 
 
@@ -65,6 +92,7 @@ def objects_without_permissions(permissions_object):
             del final_obj[perm]
     return final_obj
 
+
 def objects_with_permissions(permissions_object):
     final_obj = permissions_object.copy()
     for perm in permissions_object:
@@ -75,8 +103,6 @@ def objects_with_permissions(permissions_object):
 
 # Get users
 users = ts.user.list()
-print("Users")
-print(users)
 user_id_name_map = create_id_name_dict(users)
 
 # Get groups
@@ -84,18 +110,27 @@ groups = ts.group.list()
 group_id_name_map = create_id_name_dict(groups)
 # Permissions on liveboards
 
-# Permissions call requires GUIDs, so must get list of all of them first
-liveboards = ts.pinboard.list()
 
 answers = ts.answer.list()
 a_perms = get_permissions_for_all_objects(object_type=MetadataNames.ANSWER, listobjectheaders_response=answers)
 a_perms_with_names = map_names_to_permissions(listobjectheaders_response=answers, permissions=a_perms, users_map=user_id_name_map,
-                         groups_map=group_id_name_map)
+                        groups_map=group_id_name_map)
 
-# print(json.dumps(a_perms_with_names, indent=2))
+# Permissions call requires GUIDs, so must get list of all of them first
+liveboards = ts.pinboard.list()
 
 a_with_perms = objects_with_permissions(a_perms_with_names)
 print(json.dumps(a_with_perms, indent=2))
 
 a_without_perms = objects_without_permissions(a_perms_with_names)
 print(json.dumps(a_without_perms, indent=2))
+
+l_perms = get_permissions_for_all_objects(object_type=MetadataNames.PINBOARD, listobjectheaders_response=liveboards)
+print(l_perms)
+l_perms_with_names = map_names_to_permissions(listobjectheaders_response=liveboards, permissions=l_perms, users_map=user_id_name_map,
+                       groups_map=group_id_name_map)
+
+
+l_with_perms = objects_with_permissions(l_perms_with_names)
+print(json.dumps(l_with_perms, indent=2))
+
