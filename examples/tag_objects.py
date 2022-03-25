ea20b3e9b189b73c369f6311366f00de59d39a9b
bryanthowell-ts <bryant.howell@thoughtspot.com>
2022-03-25 14:21:38 -0500

    Added tag_objects.py to show how to find tag GUIDs and apply tags to content

---
+++ examples/tag_objects.py
@@ -0,0 +1,37 @@
+import os
+import requests.exceptions
+
+from thoughtspot import ThoughtSpot, MetadataNames
+
+# This script uses the internal metadata/delete API endpoint, which will be unnecessary once the V2 API introduces
+# a public form of delete. But it may be of use now
+
+username = os.getenv('username')  # or type in yourself
+password = os.getenv('password')  # or type in yourself
+server = os.getenv('server')        # or type in yourself
+
+ts: ThoughtSpot = ThoughtSpot(server_url=server)
+try:
+    ts.login(username=username, password=password)
+except requests.exceptions.HTTPError as e:
+    print(e)
+    print(e.response.content)
+
+
+lb_guids = ['{guid_1}', '{guid_2}']
+
+# Get tag GUIDs from a list of names
+tags_response = ts.tsrest.metadata_listobjectheaders(object_type=MetadataNames.TAG)
+tag_names_to_get_guid = ['Tag 1', 'Tag 2']
+tag_guids = []
+
+for t in tags_response:
+    if t['name'] in tag_names_to_get_guid:
+        tag_guids.append(t['id'])
+
+
+# The wrapper functions have an "assign_tags"
+ts.liveboard.assign_tags(object_guids=lb_guids, tag_guids=tag_guids)
+
+# Which are equivalent to using metadata/assigntag with the specific object_type
+ts.tsrest.metadata_assigntag(object_guids=lb_guids, object_type=MetadataNames.LIVEBOARD, tag_guids=tag_guids)
