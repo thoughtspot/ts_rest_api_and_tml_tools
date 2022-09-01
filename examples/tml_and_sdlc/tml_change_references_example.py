3c6481e5a666279c6b0bef74502de49016f2a54d
bryanthowell-ts <bryant.howell@thoughtspot.com>
2022-09-01 15:04:38 -0500

    Removed need for wrapper library, now directly using thoughtspot_rest_api_v1 library calls

--- examples/tml_and_sdlc/tml_change_references_example.py
+++ examples/tml_and_sdlc/tml_change_references_example.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from thoughtspot import *
+
+from thoughtspot_rest_api_v1 import *
 from thoughtspot_tml import *
 
 #
@@ -11,9 +12,9 @@ from thoughtspot_tml import *
 username = os.getenv('username')  # or type in yourself
 password = os.getenv('password')  # or type in yourself
 server = os.getenv('server')        # or type in yourself
-ts: ThoughtSpot = ThoughtSpot(server_url=server)
+ts: TSRestApiV1 = TSRestApiV1(server_url=server)
 try:
-    ts.login(username=username, password=password)
+    ts.session_login(username=username, password=password)
 except requests.exceptions.HTTPError as e:
     print(e)
     print(e.response.content)
@@ -27,7 +28,7 @@ except requests.exceptions.HTTPError as e:
 
 answer_guid = ''
 # Create the Answer object from TML Exported via REST API
-answer_obj = Answer(ts.tml.export_tml(guid=answer_guid))
+answer_obj = Answer(ts.metadata_tml_export(guid=answer_guid))
 
 # Alternatively, open from disk and load using YAMLTML
 # fh = open('filename.tml', 'r')
@@ -49,8 +50,8 @@ with open('{}-modified.answer.tml'.format(answer_guid), 'w', encoding='utf-8') a
 
 # Import directly to ThoughtSpot
 try:
-    import_response = ts.tml.import_tml(tml=answer_obj.tml, create_new_on_server=True)
-    new_guids = ts.tsrest.guids_from_imported_tml(import_response)
+    import_response = ts.metadata_tml_import(tml=answer_obj.tml, create_new_on_server=True)
+    new_guids = ts.guids_from_imported_tml(import_response)
     new_answer_guid = new_guids[0]
 # Some TML errors come back in the JSON response of a 200 HTTP, but a SyntaxError will be thrown
 except SyntaxError as e:
@@ -70,7 +71,7 @@ except SyntaxError as e:
 orig_ws_name_to_new_guid_map = { 'worksheet 1': 'newGuid1', 'Worksheet 2': 'newGuid2'}
 
 lb_guid = ''
-lb_obj = Liveboard(ts.tml.export_tml(guid=lb_guid))
+lb_obj = Liveboard(ts.metadata_tml_export(guid=lb_guid))
 lb_obj.remap_worksheets_to_new_fqn(name_to_guid_map=orig_ws_name_to_new_guid_map)
 
 # If you are going to import to create new, can remove the GUID for safety
@@ -82,8 +83,8 @@ with open('{}-modified.liveboard.tml'.format(lb_guid), 'w', encoding='utf-8') as
     fh.write(YAMLTML.dump_tml_object(answer_obj))
 
 # Import directly to ThoughtSpot
-import_response = ts.tml.import_tml(tml=lb_obj.tml, create_new_on_server=True)
-new_guids = ts.tsrest.guids_from_imported_tml(import_response)
+import_response = ts.metadata_tml_import(tml=lb_obj.tml, create_new_on_server=True)
+new_guids = ts.guids_from_imported_tml(import_response)
 new_lb_guid = new_guids[0]
 
 #
@@ -91,7 +92,7 @@ new_lb_guid = new_guids[0]
 #
 
 ws_guid = ""
-ws_obj = Worksheet(ts.tml.export_tml(guid=ws_guid))
+ws_obj = Worksheet(ts.metadata_tml_import(guid=ws_guid))
 # Building out this mapping is the biggest challenge. Easiest if you know the set of tables vs. trying to find in all
 orig_table_name_to_new_guid_map = { 'table name 1': 'newGuid1', 'table name 2': 'newGuid2'}
 ws_obj.remap_tables_to_new_fqn(name_to_fqn_map=orig_table_name_to_new_guid_map)
@@ -106,8 +107,8 @@ with open('{}-modified.worksheet.tml'.format(ws_guid), 'w', encoding='utf-8') as
 
 # Import directly to ThoughtSpot
 try:
-    import_response = ts.tml.import_tml(tml=ws_obj.tml, create_new_on_server=True)
-    new_guids = ts.tsrest.guids_from_imported_tml(import_response)
+    import_response = ts.metadata_tml_import(tml=ws_obj.tml, create_new_on_server=True)
+    new_guids = ts.guids_from_imported_tml(import_response)
     new_ws_guid = new_guids[0]
 # Some TML errors come back in the JSON response of a 200 HTTP, but a SyntaxError will be thrown
 except SyntaxError as e:
@@ -126,7 +127,7 @@ except SyntaxError as e:
 # You can also modify columns or try and build from scratch (see tml_from_scratch.py example)
 
 table_guid = ""
-table_obj = Table(ts.tml.export_tml(guid=table_guid))
+table_obj = Table(ts.metadata_tml_export(guid=table_guid))
 # Connection names are unique and don't require a GUID ever
 table_obj.connection_name = 'New Connection Name'
 table_obj.db_name = 'NEW_DB_NAME'
@@ -143,8 +144,8 @@ with open('{}-modified.table.tml'.format(ws_guid), 'w', encoding='utf-8') as fh:
 
 # Import directly to ThoughtSpot
 try:
-    import_response = ts.tml.import_tml(tml=table_obj.tml, create_new_on_server=True)
-    new_guids = ts.tsrest.guids_from_imported_tml(import_response)
+    import_response = ts.metadata_tml_import(tml=table_obj.tml, create_new_on_server=True)
+    new_guids = ts.guids_from_imported_tml(import_response)
     new_table_guid = new_guids[0]
 
 # Some TML errors come back in the JSON response of a 200 HTTP, but a SyntaxError will be thrown
